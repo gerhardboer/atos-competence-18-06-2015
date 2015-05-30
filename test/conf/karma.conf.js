c7815f92fe9c6338237988682d303641d8134b14
Gerhard Boer <gghboer@gmail.com>
2015-05-30 16:21:40 +0200

    added karma-ng-html2js-preprocessor for the templates added unit tests

--- test/conf/karma.conf.js
+++ test/conf/karma.conf.js
@@ -1,6 +1,10 @@
 module.exports = function (config) {
     config.set({
 
+        preprocessors: {
+            'app/src/users/**/*.html': ['ng-html2js']
+        },
+
         basePath: '../../',
         urlRoot: '/',
 
@@ -9,9 +13,11 @@ module.exports = function (config) {
             'app/bower_components/angular-aria/angular-aria.js',
             'app/bower_components/angular-mocks/angular-mocks.js',
             'app/bower_components/angular-material/angular-material.js',
+            'app/bower_components/angular-animate/angular-animate.js',
             'app/src/main.js',
             'app/src/**/*.js',
-            'test/**/*.spec.js'
+            'test/**/*.spec.js',
+            'app/src/users/**/*.html'
         ],
 
         autoWatch: true,
@@ -21,6 +27,7 @@ module.exports = function (config) {
         browsers: ['PhantomJS'],
 
         plugins: [
+            'karma-ng-html2js-preprocessor',
             'karma-phantomjs-launcher',
             'karma-jasmine',
             'karma-junit-reporter'
@@ -29,6 +36,14 @@ module.exports = function (config) {
         junitReporter: {
             outputFile: 'test_out/unit.xml',
             suite: 'unit'
+        },
+
+        ngHtml2JsPreprocessor: {
+            // strip this from the file path
+            stripPrefix: 'app/',
+            // setting this option will create only a single module that contains templates
+            // from all the files, so you can load them all with module('foo')
+            moduleName: 'user.templates'
         }
 
     });
